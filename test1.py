56e3e31348a141b8372a42db6425c081da16ba2b
HitakaK <renkagamine0703@icloud.com>
2024-11-30 22:37:57 +0900

    整理2

--- test1.py
+++ test1.py
@@ -1,17 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
-service = ChromeService(executable_path=ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 
 # disable the AutomationControlled feature of Blink rendering engine
 options.add_argument('--disable-blink-features=AutomationControlled')
 
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(options=options)
 
 # OpenAIのウェブサイトにアクセス
 driver.get('https://p.eagate.573.jp/game/ddr/ddrworld/login.html')
-# driver.get('https://www.google.com')
 
 input("test...")
