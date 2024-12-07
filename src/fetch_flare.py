256edc6b9308e7a5b2b34b405c683e3857e29e58
HitakaK <renkagamine0703@icloud.com>
2024-12-07 15:02:24 +0900

    不要ファイルの削除

--- src/fetch_flare.py
+++ src/fetch_flare.py
@@ -1,48 +1,85 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import pickle
-from flare_output import flare_to_csv
+from bs4 import BeautifulSoup
+from flare_rank import flare_with_rank
+from load_cookie import get_page_with_cookies
 
-# Requestsでデータを操作
-home_url = 'https://p.eagate.573.jp/game/ddr/ddrworld/top/index.html'
-target_url = 'https://p.eagate.573.jp/game/ddr/ddrworld/playdata/flare_data_single.html'
+def get_tag_content_by_id(html, tag_id):
+    soup = BeautifulSoup(html, 'lxml')
+    # 指定されたIDを持つタグを検索
+    # tag = soup.find(id=tag_id)
+    # tags = soup.find_all(id=tag_id)
 
-options = webdriver.ChromeOptions()
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument("--headless")
+    # if tag:
+    #     return tag.text.strip()
+    # if tags:
+    #     return [tag.text.strip().split() for tag in tags]
+    # else:
+    #     print(f"Tag with ID {tag_id} not found.")
+    #     return None
+    # 結果を格納するリスト
+    # IDを持つ全ての要素をリストで取得
+    elements = soup.find_all(id=tag_id)
+    
+    results = []
+    
+    for tag in elements:
+        result = []
+        # タグ内のテキストを <br> で分割
+        text_parts = tag.get_text(separator="\n", strip=True).split("\n")
+        
+        # 分割されたテキスト部分を結果リストに追加
+        result.extend(text_parts)
+        # print(result)
+        results.append(result)
 
-driver2 = webdriver.Chrome(options=options)
+    # print(results)
+    return results
+    
+def get_tag_content_by_classname(html, tag_class):
+    soup = BeautifulSoup(html, 'lxml')
+    # 指定されたIDを持つタグを検索
+    # tag = soup.find(id=tag_id)
+    tags = soup.find_all(class_=tag_class)
 
-driver2.get(home_url)
+    if tags:
+        return [tag.text.strip().split() for tag in tags]
+    else:
+        print(f"Tag with ID {tag_class} not found.")
+        return None
 
-# Seleniumから取得したクッキーをRequestsのセッションにセット
-cookie_file_name = "cookie.pkl"
-with open(cookie_file_name, 'rb') as cookie_file:
-    cookies = pickle.load(cookie_file)
+if __name__ == "__main__":
+    # 使用例
+    cookie_file = './cookie.pkl'  # 保存したクッキーのファイル
+    url = 'https://p.eagate.573.jp/game/ddr/ddrworld/playdata/flare_data_single.html'  # アクセスしたいURL
+    version_name_id = 'graph_title'
+    table_id = 'data_tbl'  # 取得したいタグのID
 
-for cookie in cookies:
-    cookie_dict = {
-            'name': cookie['name'],
-            'value': cookie['value']
-        }
-    driver2.add_cookie(cookie_dict)
+    # ページを取得
+    session = get_page_with_cookies(cookie_file)
+    response = session.get(url)
 
-driver2.get(target_url)
+    html = response.text
 
-tables = driver2.find_elements(By.ID, "data_tbl")
-titles = driver2.find_elements(By.CLASS_NAME, "graph_title")
-# print(tables.text)
+    if html:
+        # IDを指定してタグの内容を取得
+        # content = get_tag_content_by_id(html, tag_id)
+        versions = get_tag_content_by_classname(html, version_name_id)
+        tables = get_tag_content_by_id(html, table_id)
 
-out_file_name = "row_out_flare.txt"
+        if tables:
+            idx = 0
 
-# contents = driver2.page_source
-with open(out_file_name, "w", encoding="utf-8") as out_file:
-    for title, table in zip(titles, tables):
-        out_file.write(title.text+"\n")
-        out_file.write(table.text+"\n")
-        out_file.write("-" * 20+"\n")
-
-driver2.quit()
-
-flare_to_csv(out_file_name)
+            out_file_name = "flare_output.csv"
+            with open(out_file_name, "w", encoding="utf-8") as out_file:
+                for version, table in zip(versions, tables):
+                    #print(table)
+                    if idx == 0:
+                        # print("バージョン;楽曲名;難易度;レベル;フレアランク;フレアスキル;プレー日時\n")
+                        out_file.write("バージョン;楽曲名;難易度;レベル;フレアランク;フレアスキル;プレー日時\n")
+                    for i in range(1, int((len(table) - 1) / 5)):
+                        idx += 1
+                        chart_lv = table[5 * i + 3]
+                        lv = chart_lv.split(".")[1]
+                        flare_skill = table[5 * i + 4]
+                        rank = flare_with_rank(int(lv), int(flare_skill))
+                        # print(f"{version[0]};{table[5 * i + 1]};{table[5 * i + 2]};{table[5 * i + 3]};{table[5 * i + 4]};{table[5 * i + 5]}\n")
+                        out_file.write(f"{version[0]};{table[5 * i + 1]};{table[5 * i + 2]};{chart_lv};{rank};{flare_skill};{table[5 * i + 5]}\n")
