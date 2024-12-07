ae8c7203260a7932f7dc158e78d0818b1eff4919
Maruhati3 <hatigiku@gmail.com>
2024-12-07 17:07:32 +0900

    ver付きのデータダウンロード機能の実装

--- src/get_songlv_data.py
+++ src/get_songlv_data.py
@@ -21,70 +21,77 @@ def TuneCrump(Crump):
     return tmp
 
 def data_asong(session):
-    allscore_url='https://p.eagate.573.jp/game/ddr/ddrworld/playdata/music_data_single.html?offset=0&filter=0&filtertype=0&display=flareskill'
+    allscore_url='https://p.eagate.573.jp/game/ddr/ddrworld/playdata/music_data_single.html?offset=4&filter=7&filtertype=18&display=score'
     baseallsocre_url1='https://p.eagate.573.jp/game/ddr/ddrworld/playdata/music_data_single.html?offset='
-
-    baseallscore_url3=      '&filter=0&filtertype=0&display=score'
+    baseallscore_url3=      '&filter=7&filtertype='
+    baseallscore_url5=      '&display=score'
     baseallsocre_url_rival1='https://p.eagate.573.jp/game/ddr/ddrworld/rival/music_data_single.html?offset='
     baseallscore_url_rival3= '&filter=0&filtertype=0&display=score&rival_id=71393284'
-    csvfilename='asongpage.csv'
+    #offset:=ページ
+    #filtertype:=バージョン
+
+    csvfilename='songpage_withVer.csv'
     with open(csvfilename,'w',encoding="utf-8") as fname:
-        offsetindex=0
-        fname.write("SPDP;曲名;難易度;Lv;SCORE;Crank;Crump;Fskill;Frank\n")
-        #初回だけlen(alltr)の値を取得するためにwhileの外で実行
-        index_url=baseallsocre_url1+str(offsetindex)+baseallscore_url3
-        print('='*50)
-        response = session.get(index_url)
-        print("取得したデータ一覧:")
-        print(index_url)
-        print(response.url)
-        soup=BeautifulSoup(response.content,'lxml')
-        alltr=soup.find_all("tr",class_="data")
+        
+        fname.write("SPDP;Version;曲名;難易度;Lv;SCORE;Crank;Crump;Fskill;Frank\n")
+        NumOfVer=[0,0,0,0,0, 0,0,0,1,0, 1,1,1,1,2, 2,1,2,3,4]
+        for filtertypeindex in range(0,20):
             
-        while len(alltr)!=0:
-            
-            print(len(alltr))
-            for songindex in range(len(alltr)):
-                alltd=alltr[songindex].find_all("td")
-                print("曲名:",alltd[0].find_all("a")[0].text)
-                for c in range(1,6):
-                    Songname=alltd[0].find_all("a")[0].text
-                    print("曲名:",Songname)
-                    Difficulty=alltd[c]['id']
-                    print("\n"+Difficulty+"="*20)
-                    if alltd[c].find("a"):
-                        diff=alltd[c].find("a")
-                        SCORE=diff.find_all("div")[0].text
-                        if SCORE=='---':
-                            SCORE='0'
-                        print("SCORE: "+SCORE)
-                        Crank=diff.find_all("div")[1].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
-                        Crank=Crank.split("rank_s_")[1].replace("_p","+").replace("_m","-").upper()
-                        print("ClearRank: "+Crank)
-                        Crump=diff.find_all("div")[2].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
-                        Crump=TuneCrump(Crump)
+            print('version='+str(filtertypeindex))
 
-                        print("ClearRump:",Crump)
-                        Fskill=diff.find_all("div")[3].text
-                        if Fskill=='---':
-                            Fskill='0'
-                        print("F-Skill:"+Fskill)
-                        Frank=diff.find_all("div")[4].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
-                        Frank=Frank.split("flare_")[1].upper()
-                        print("F-Rank:"+Frank)
-                        fname.write(f"SP;{Songname};{Difficulty};Lv;{SCORE};{Crank};{Crump};{Fskill};{Frank}\n")
-                    else:
-                        print("NoSong")
-            offsetindex+=1
+            #各バージョンのページを読み込み
+            for offsetindex in range(0,NumOfVer[filtertypeindex]+1):
+                #読み込むURLを設定
+                index_url=baseallsocre_url1+str(offsetindex)+baseallscore_url3+str(filtertypeindex)+baseallscore_url5
+                print('='*50)
+                response = session.get(index_url)
+                print("取得したデータ一覧:")
+                print(index_url)
+                print(response.url)
+                #ページのhtml全体を取得
+                soup=BeautifulSoup(response.content,'lxml')
+                alltr=soup.find_all("tr",class_="data") 
 
-            index_url=baseallsocre_url1+str(offsetindex)+baseallscore_url3
-            print('='*50)
-            response = session.get(index_url)
-            print("取得したデータ一覧:")
-            print(index_url)
-            print(response.url)
-            soup=BeautifulSoup(response.content,'lxml')
-            alltr=soup.find_all("tr",class_="data")
+                print(len(alltr))
+                if len(alltr)<=0:
+                    break
+
+
+                for songindex in range(len(alltr)):
+                    alltd=alltr[songindex].find_all("td")
+                    #print("曲名:",alltd[0].find_all("a")[0].text)
+                    for c in range(1,6):
+                        Songname=alltd[0].find_all("a")[0].text
+                        #print("曲名:",Songname)
+                        Difficulty=alltd[c]['id']
+                        #print("\n"+Difficulty+"="*20)
+                        if alltd[c].find("a"):
+                            diff=alltd[c].find("a")
+                            SCORE=diff.find_all("div")[0].text
+                            if SCORE=='---':
+                                SCORE='0'
+                            #print("SCORE: "+SCORE)
+                            Crank=diff.find_all("div")[1].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                            Crank=Crank.split("rank_s_")[1].replace("_p","+").replace("_m","-").upper()
+                            #print("ClearRank: "+Crank)
+                            Crump=diff.find_all("div")[2].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                            Crump=TuneCrump(Crump)
+
+                            #print("ClearRump:",Crump)
+                            Fskill=diff.find_all("div")[3].text
+                            if Fskill=='---':
+                                Fskill='0'
+                            #print("F-Skill:"+Fskill)
+                            Frank=diff.find_all("div")[4].find("img")['src'].rsplit('/', 1)[-1].rsplit('.', 1)[0]
+                            Frank=Frank.split("flare_")[1].upper()
+                            #print("F-Rank:"+Frank)
+                            fname.write(f"SP;{filtertypeindex};{Songname};{Difficulty};Lv;{SCORE};{Crank};{Crump};{Fskill};{Frank}\n")
+                        else:
+                            print("NoSong")
+
+                
+
+
                         
             
 
